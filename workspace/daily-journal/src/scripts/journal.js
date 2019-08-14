0d2e67defee9072e62db343c59f1c92e93ccf1c2
Your Name <lukemiller4779@gmail.com>
2019-08-14 15:03:04 -0400

    ~/workspace/daily-journal/src

---
+++ workspace/daily-journal/src/scripts/journal.js
@@ -0,0 +1,15 @@
+//color pickers
+
+const journalEntry = {
+    date:"journalDate",
+    conceptsCovered:"javascript conditionals",
+    journalEntry:"Computer logic operations",
+    Mood:"melancholy",
+
+
+}
+console.log(journalEntry)
+var arrayofjournalentries = [
+  ]
+arrayofjournalentries.push(journalEntry);
+console.log(arrayofjournalentries)
